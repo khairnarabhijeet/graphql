6b17cf6abfe94b6958efd9929ba6b2f345be3ffc
khairnarabhijeet <47552955+khairnarabhijeet@users.noreply.github.com>
2025-04-07 11:24:03 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,1 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(BookRepository repo) {
+        return args -> {
+            repo.save(new Book("Spring Boot GraphQL", "Abhijeet", 499.99));
+            repo.save(new Book("Effective Java", "Joshua Bloch", 799.99));
+        };
+    }
+}
 
